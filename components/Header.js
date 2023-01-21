bd477a6397a0d1f878d2056167d1e631a58c3265
ramadhani151221 <202410101050@mail.unej.ac.id>
2023-01-21 13:04:56 +0700

    add logout feature

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,66 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { getCookie, deleteCookie } from "cookies-next";
 import React from "react";
 import Logo from "public/logo.png";
 import Image from "next/image";
+import apiConnection from "@/apiConnection";
 
 export default function Header() {
+  const [user, setUser] = useState({});
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.asPath !== router.route) {
+    }
+    getUser();
+  }, [router]);
+
+  const token = getCookie("token");
+
+  const getUser = async () => {
+    try {
+      await apiClient
+        .get(`/api/user`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((response) => {
+          const user = response.data;
+          setUser(user);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+
+    try {
+      await apiConnection
+        .delete(`/api/user/logout`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((response) => {
+          const data = response.data;
+          deleteCookie("token");
+          router.push("/login");
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className="flex justify-between items-center h-12 bg-blue-500 text-white px-10">
-      <div className="flex flex-row">
+    <div className="flex justify-between items-center h-14 bg-blue-500 text-white px-10 shadow-lg">
+      <div className="flex flex-row shadow-lg">
         <Image src={Logo} width={40} height={40} alt={Logo} />
         <p className="font-bold text-xl ml-3">Yusuf Library</p>
       </div>
       <div>
-        <a href="/logout">Logout</a>
+        <button 
+          type="button"
+          onClick={(e) => handleLogout(e)}
+        >Logout</button>
       </div>
     </div>
   );
